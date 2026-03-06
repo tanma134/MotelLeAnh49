4118c4fe16d534982286ba4bd1f779d6881bfc49
tanma134 <tanmnce182352@fpt.edu.vn>
2026-03-06 10:30:50 +0700

    Update database

---
+++ DataAccess/Models/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Password { get; set; }
+
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        [MaxLength(20)]
+        public string Role { get; set; } = "Customer";
+
+        public bool IsActive { get; set; } = true;
+
+        // Navigation
+        public Customer Customer { get; set; }
+    }
+}
---
+++ DataAccess/Models/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Account")]
+        public int AccountId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FullName { get; set; }
+
+        [MaxLength(20)]
+        public string Phone { get; set; }
+
+        [MaxLength(200)]
+        public string Address { get; set; }
+
+        [MaxLength(50)]
+        public string IdentityNumber { get; set; }
+
+        // Navigation
+        public Account Account { get; set; }
+    }
+}
